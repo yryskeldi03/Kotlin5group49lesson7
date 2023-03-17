802600d4bfa2a02849fd18cc3b3a6b915ccb0314
ymajrambekov@gmail.com <ymajrambekov@gmail.com>
2023-03-17 19:35:46 +0600

    sum

--- app/src/main/java/com/example/kotlin5group49lesson7/MainActivity.kt
+++ app/src/main/java/com/example/kotlin5group49lesson7/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.kotlin5group49lesson7
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    var a = 2
+    var b = 4
+    var resultat = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        resultat = a + b
     }
 }
